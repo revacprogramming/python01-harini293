315f3913e8500a7e63f26ab1fa849516fe2e41bf
harini293 <93110552+harini293@users.noreply.github.com>
2022-06-08 20:08:44 +0530

    done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,13 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname,'r')
+string=list()
+for line in fh:
+    content=line.split()
+    for word in content:
+        if not word in string:
+           string.append(word)
+string.sort()
+print(string)
+    
