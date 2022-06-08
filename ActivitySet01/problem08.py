5fa30b01d8658cd3a49342bede81c2349a1822d8
harini293 <93110552+harini293@users.noreply.github.com>
2022-06-08 20:09:27 +0530

    done

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,15 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+fname = input('enter file name : ')
+fhandle = open(fname,'r')
+content = fhandle.read()
+count,total=0,0
+for line in fhandle:
+    line = line.rstrip()
+    if line.startswith('X-DSPAM-Confidence:'):
+        text=line.split(":")
+        y=float(text[1])
+        total= total+y
+        count+=1
+avg = total/count
+print('Average spam confidence:',avg)
