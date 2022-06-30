66c68c73e2ae7408ddcbe6f314536ccf1cfa6d29
harini293 <93110552+harini293@users.noreply.github.com>
2022-06-30 23:11:03 +0530

    Update problem09.py

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -5,7 +5,7 @@ fh = open(fname,'r')
 string=list()
 for line in fh:
     content=line.split()
-    for word in content:
+    for word in content: 
         if not word in string:
            string.append(word)
 string.sort()
