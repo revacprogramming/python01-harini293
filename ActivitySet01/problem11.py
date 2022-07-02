c2d83e80ffeec7ce0a89683e22576bd36ea49ce7
harini293 <93110552+harini293@users.noreply.github.com>
2022-07-02 22:19:18 +0530

    Update problem11.py

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,21 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+fname=input('enter file name :')
+fhandle=open(fname,'r')
+hours=[]
+count=dict()
+for line in fhandle:
+    if line.startswith('From '):
+        lst=line.split()
+        time=lst[5]
+        time=time.split(':')
+        hours.append(time[0])
+for num in hours:
+    if num not in count:
+        count[num]=1
+    else :
+        count[num]+=1
+t=list(count.items())
+t.sort()
+for key,value in t:
+    print(key,value)
