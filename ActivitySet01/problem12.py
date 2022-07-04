bb3eeab392099649170be85cf568fe8b399b9fe6
harini293 <93110552+harini293@users.noreply.github.com>
2022-07-04 15:12:12 +0530

    Update problem12.py

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,16 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+fname=input('enter file name')
+fhandle=open(fname,'r')
+sum=0
+count=0
+for line in fhandle:
+    line=line.rstrip()
+    num=re.findall('[0-9]+',line)
+    if len(num)>0:
+        for value in num:
+            count+=1
+            sum=int(value)+sum
+print(sum)
+print(count)
