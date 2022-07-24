d965d5f8c6ee7d5db3e4a76e98b0b90ef9284e8a
harini293 <93110552+harini293@users.noreply.github.com>
2022-07-24 08:18:38 +0530

    Update problem01.py

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -1,20 +1,14 @@
-def add(a,b):
+def inp():
+    a = float(input('enter a number: '))
+    return a
+
+
+def add(a, b):
     return (a+b)
-    
 
-def output(a, b):
-    print(a,'+',b,'is',sum)
-
-def input_two_numbers():
-    a=int(input('enter a number'))
-    b=int(input('enter a number'))
-    return(a,b)
 
 def main():
-    a, b = input_two_numbers()
-    sum = add(a, b)
-    output(a, b)
-    print(sum)
-
-if __name__ == '__main__':
-    main()
+    a = inp()
+    b = inp()
+    c = add(a, b)
+    print(c)
