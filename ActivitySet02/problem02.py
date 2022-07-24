4e0194f7f810059f01facd29a6f0a8b50473dd76
harini293 <93110552+harini293@users.noreply.github.com>
2022-07-24 08:19:01 +0530

    Update problem02.py

--- ActivitySet02/problem02.py
+++ ActivitySet02/problem02.py
@@ -1,18 +1,20 @@
+def add(a,b):
+    return (a+b)
+    
 
-def add(a, b):
-    pass  # ...
+def output(a, b):
+    print(a,'+',b,'is',sum)
 
-
-def output(a, b, sum):
-    pass  # ...
-
+def input_two_numbers():
+    a=int(input('enter a number'))
+    b=int(input('enter a number'))
+    return(a,b)
 
 def main():
     a, b = input_two_numbers()
     sum = add(a, b)
-
-    output(a, b, sum)
-
+    output(a, b)
+    print(sum)
 
 if __name__ == '__main__':
     main()
